a3599e3d74c0b0fb26e9395a2608bf9a7c74b1a9
Benjamin Ting <chbting@gmail.com>
2025-11-15 13:53:37 +0800

    Added BusFareSchema

--- lib/isar_manager.dart
+++ lib/isar_manager.dart
@@ -5,9 +5,10 @@ import 'package:flutter/services.dart';
 import 'package:isar_community/isar.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:up_bus_hk_core/isar/data_builder_models/bus_fare.dart';
+import 'package:up_bus_hk_core/isar/data_builder_models/company_bus_route.dart';
 import 'package:up_bus_hk_core/isar/models/bus_route.dart';
 import 'package:up_bus_hk_core/isar/models/bus_stop.dart';
-import 'package:up_bus_hk_core/isar/models/company_bus_route.dart';
 import 'package:up_bus_hk_core/isar/models/minibus_route.dart';
 import 'package:up_bus_hk_core/isar/models/minibus_stop.dart';
 import 'package:up_bus_hk_core/isar/models/track.dart';
@@ -27,6 +28,7 @@ class IsarManager {
     await Isar.open(
       [
         CompanyBusRouteSchema,
+        BusFareSchema,
         BusRouteSchema,
         BusStopSchema,
         MinibusRouteSchema,
